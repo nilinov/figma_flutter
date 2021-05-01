a59768745ef84f3bacda2055cfbf7d36876da1e8
Nicolay i <nic.ilinov@gmail.com>
2021-05-01 17:10:37 +0700

    ++

--- server.dart
+++ server.dart
@@ -1,36 +1,43 @@
 import 'dart:io';
 
+import 'package:process_run/shell.dart';
+
 Future main() async {
   var server = await HttpServer.bind(
     InternetAddress.loopbackIPv4,
     4040,
   );
   print('Listening on localhost:${server.port}');
+  var shell = Shell();
 
   await for (HttpRequest request in server) {
-    switch (request.uri.toString()) {
-      case '/open_view':
-        request.response.write('open view mode');
-        Process.run('chmod +x open_screen.sh && ./open_screen.sh', []);
-        break;
-      case '/start':
-        request.response.write('start dev server');
-        Process.run('chmod +x start.sh && ./start.sh', []);
-        break;
-      case '/reload':
-        request.response.write('start reload');
-        Process.run('chmod +x reload.sh && ./reload.sh', []);
-        break;
-      case '/restart':
-        request.response.write('start restart');
-        Process.run('chmod +x restart.sh && ./restart.sh', []);
-        break;
-      case '/install':
-        request.response.write('start install');
-        Process.run('chmod +x install.sh && ./install.sh', []);
-        break;
-      default:
-        request.response.write('no found command');
+    try {
+      switch (request.uri.toString()) {
+        case '/open_view':
+          request.response.write('open view mode');
+          shell.run("screen -d -m -S ServerFault");
+          break;
+        case '/start':
+          request.response.write('start dev server');
+          shell.run("flutter run -d web-server --web-port 3000");
+          break;
+        case '/reload':
+          request.response.write('start reload');
+          Process.run('chmod +x reload.sh && ./reload.sh', []);
+          break;
+        case '/restart':
+          request.response.write('start restart');
+          Process.run('chmod +x restart.sh && ./restart.sh', []);
+          break;
+        case '/install':
+          request.response.write('start install');
+          Process.run('chmod +x install.sh && ./install.sh', []);
+          break;
+        default:
+          request.response.write('no found command');
+      }
+    } catch(e) {
+      request.response.write('error ${e.toString()}');
     }
 
     await request.response.close();
