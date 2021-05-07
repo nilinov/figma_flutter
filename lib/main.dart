e87fe9354a0837e3c659b99c71cb019923f7ff04
Nicolay i <nic.ilinov@gmail.com>
2021-05-07 09:37:22 +0700

    update destop

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    print('renderer');
+
     return Scaffold(
       body: Center(
         child: Column(
