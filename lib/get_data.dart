cba453dd4ff4b79cd9cbd6afdda81dbef5de8bcd
nilinov <72597726+nilinov@users.noreply.github.com>
2021-05-12 14:09:26 +0700

    fix borderradius

--- lib/get_data.dart
+++ lib/get_data.dart
@@ -8,8 +8,7 @@ Future<String> getData() async {
     return '''
 
 
-{"type":"FRAME","id":"121:9","parent":{"id":"105:50"},"name":"Frame 3","removed":false,"visible":true,"locked":false,"opacity":1,"blendMode":"PASS_THROUGH","isMask":false,"effects":[],"effectStyleId":"","relativeTransform":[[1,0,5],[0,1,132]],"absoluteTransform":[[1,0,1078],[0,1,404]],"x":5,"y":132,"width":271,"height":48,"rotation":0,"layoutAlign":"INHERIT","constrainProportions":false,"layoutGrow":0,"children":[{"type":"TEXT","id":"105:23","parent":{"id":"121:9"},"name":"Apply","removed":false,"visible":true,"locked":false,"opacity":1,"blendMode":"PASS_THROUGH","isMask":false,"effects":[],"effectStyleId":"","fills":[{"type":"SOLID","visible":true,"opacity":1,"blendMode":"NORMAL","color":{"r":0.95686274766922,"g":0.9529411792755127,"b":0.9411764740943909}}],"fillStyleId":"S:523a244e30fac4b9f66e4093e4cd7fcad3e9a477,770:14","strokes":[],"strokeStyleId":"","strokeWeight":1,"strokeAlign":"OUTSIDE","strokeCap":"NONE","strokeJoin":"MITER","strokeMiterLimit":4,"dashPattern":[],"relativeTransform":[[1,0,10],[0,1,13]],"absoluteTransform":[[1,0,1088],[0,1,417]],"x":10,"y":13,"width":251,"height":22,"rotation":0,"layoutAlign":"INHERIT","constrainProportions":false,"layoutGrow":1,"exportSettings":[],"constraints":{"horizontal":"CENTER","vertical":"CENTER"},"textStyleId":"","characters":"Apply","hasMissingFont":false,"autoRename":true,"fontSize":16,"paragraphIndent":0,"paragraphSpacing":0,"textAlignHorizontal":"CENTER","textAlignVertical":"CENTER","textCase":"ORIGINAL","textDecoration":"NONE","textAutoResize":"HEIGHT","letterSpacing":{"unit":"PIXELS","value":0},"lineHeight":{"unit":"PERCENT","value":139.9999976158142},"fontName":{"family":"Roboto","style":"Bold"},"reactions":[]}],"exportSettings":[],"fills":[{"type":"SOLID","visible":true,"opacity":1,"blendMode":"NORMAL","color":{"r":0.33725491166114807,"g":0.5372549295425415,"b":0.7529411911964417}}],"fillStyleId":"S:237336b54608a6cbff3e533a3e48da4a5eb9098c,1604:1","strokes":[],"strokeStyleId":"","strokeWeight":1,"strokeAlign":"INSIDE","strokeCap":"NONE","strokeJoin":"MITER","strokeMiterLimit":4,"dashPattern":[],"cornerRadius":8,"cornerSmoothing":0,"topLeftRadius":8,"topRightRadius":8,"bottomLeftRadius":8,"bottomRightRadius":8,"paddingLeft":10,"paddingRight":10,"paddingTop":13,"paddingBottom":13,"primaryAxisAlignItems":"CENTER","counterAxisAlignItems":"CENTER","primaryAxisSizingMode":"FIXED","layoutGrids":[],"gridStyleId":"","backgrounds":[{"type":"SOLID","visible":true,"opacity":1,"blendMode":"NORMAL","color":{"r":0.33725491166114807,"g":0.5372549295425415,"b":0.7529411911964417}}],"backgroundStyleId":"S:237336b54608a6cbff3e533a3e48da4a5eb9098c,1604:1","clipsContent":false,"guides":[],"expanded":true,"constraints":{"horizontal":"CENTER","vertical":"CENTER"},"layoutMode":"HORIZONTAL","counterAxisSizingMode":"AUTO","horizontalPadding":10,"verticalPadding":13,"itemSpacing":10,"overflowDirection":"NONE","numberOfFixedChildren":0,"overlayPositionType":"CENTER","overlayBackground":{"type":"NONE"},"overlayBackgroundInteraction":"NONE","reactions":[]}
-
+{"type":"FRAME","id":"105:16","parent":{"id":"0:1"},"name":"Tab_1","removed":false,"visible":true,"locked":false,"opacity":1,"blendMode":"PASS_THROUGH","isMask":false,"effects":[],"effectStyleId":"","relativeTransform":[[1,0,828],[0,1,365]],"absoluteTransform":[[1,0,828],[0,1,365]],"x":828,"y":365,"width":223,"height":50,"rotation":0,"constrainProportions":false,"layoutGrow":0,"children":[{"type":"TEXT","id":"105:17","parent":{"id":"105:16"},"name":"Traveling","removed":false,"visible":true,"locked":false,"opacity":1,"blendMode":"PASS_THROUGH","isMask":false,"effects":[],"effectStyleId":"","fills":[{"type":"SOLID","visible":true,"opacity":1,"blendMode":"NORMAL","color":{"r":1,"g":1,"b":1}}],"fillStyleId":"","strokes":[],"strokeStyleId":"","strokeWeight":1,"strokeAlign":"OUTSIDE","strokeCap":"NONE","strokeJoin":"MITER","strokeMiterLimit":4,"dashPattern":[],"relativeTransform":[[1,0,79],[0,1,5]],"absoluteTransform":[[1,0,907],[0,1,370]],"x":79,"y":5,"width":65,"height":40,"rotation":0,"layoutAlign":"INHERIT","constrainProportions":false,"layoutGrow":0,"exportSettings":[],"constraints":{"horizontal":"CENTER","vertical":"CENTER"},"textStyleId":"","characters":"Traveling","hasMissingFont":false,"autoRename":true,"fontSize":16,"paragraphIndent":0,"paragraphSpacing":0,"textAlignHorizontal":"CENTER","textAlignVertical":"CENTER","textCase":"ORIGINAL","textDecoration":"NONE","textAutoResize":"NONE","letterSpacing":{"unit":"PERCENT","value":0},"lineHeight":{"unit":"PIXELS","value":30},"fontName":{"family":"Roboto","style":"Regular"},"reactions":[]}],"exportSettings":[],"fills":[{"type":"SOLID","visible":true,"opacity":1,"blendMode":"NORMAL","color":{"r":0.2705882489681244,"g":0.2823529541492462,"b":0.30980393290519714}}],"fillStyleId":"","strokes":[],"strokeStyleId":"","strokeWeight":1,"strokeAlign":"INSIDE","strokeCap":"NONE","strokeJoin":"MITER","strokeMiterLimit":4,"dashPattern":[],"cornerRadius":20,"cornerSmoothing":0,"topLeftRadius":20,"topRightRadius":20,"bottomLeftRadius":20,"bottomRightRadius":20,"paddingLeft":20,"paddingRight":20,"paddingTop":0,"paddingBottom":0,"primaryAxisAlignItems":"CENTER","counterAxisAlignItems":"CENTER","primaryAxisSizingMode":"FIXED","layoutGrids":[],"gridStyleId":"","backgrounds":[{"type":"SOLID","visible":true,"opacity":1,"blendMode":"NORMAL","color":{"r":0.2705882489681244,"g":0.2823529541492462,"b":0.30980393290519714}}],"backgroundStyleId":"","clipsContent":false,"guides":[],"expanded":true,"constraints":{"horizontal":"MIN","vertical":"MIN"},"layoutMode":"HORIZONTAL","counterAxisSizingMode":"FIXED","horizontalPadding":20,"verticalPadding":0,"itemSpacing":10,"overflowDirection":"NONE","numberOfFixedChildren":0,"overlayPositionType":"CENTER","overlayBackground":{"type":"NONE"},"overlayBackgroundInteraction":"NONE","reactions":[]}
 
 
 ''';
@@ -41,6 +40,7 @@ Widget getWidgetByMap(Map<String, dynamic> json) {
           // color: getColorFromFills(json),
           color: Colors.white,
           border: getBorder(json),
+          borderRadius: getBorderRadius(json),
         ),
         child: getChildrenByLayoutMode(json),
       );
@@ -51,6 +51,7 @@ Widget getWidgetByMap(Map<String, dynamic> json) {
         decoration: BoxDecoration(
           color: getColorFromFills(json),
           border: getBorder(json),
+          borderRadius: getBorderRadius(json),
         ),
         child: getChildrenByLayoutMode(json),
       );
@@ -58,9 +59,12 @@ Widget getWidgetByMap(Map<String, dynamic> json) {
       return getText(json);
     case 'FRAME':
       return Container(
+        width: json['width'],
+        height: json['height'],
         decoration: BoxDecoration(
           color: getColorFromFills(json),
           border: getBorder(json),
+          borderRadius: getBorderRadius(json),
         ),
         padding: getPadding(json),
         child: getChildrenByLayoutMode(json),
@@ -74,6 +78,7 @@ Widget getWidgetByMap(Map<String, dynamic> json) {
         decoration: BoxDecoration(
           color: getColorFromFills(json),
           border: getBorder(json),
+          borderRadius: getBorderRadius(json),
         ),
         padding: getPadding(json),
         child: items,
@@ -106,6 +111,18 @@ Border getBorder(Map<String, dynamic> json) {
   }
 
   return Border.all(width: 0, color: Colors.transparent);
+}
+
+BorderRadius getBorderRadius(Map<String, dynamic> json) {
+  print('getBorderRadius');
+  if (json['topLeftRadius'] == null) return null;
+
+  return BorderRadius.only(
+    topLeft: Radius.circular(json['topLeftRadius'] ?? 0),
+    topRight: Radius.circular(json['topRightRadius'] ?? 0),
+    bottomLeft: Radius.circular(json['bottomLeftRadius'] ?? 0),
+    bottomRight: Radius.circular(json['bottomRightRadius'] ?? 0),
+  );
 }
 
 Widget getText(Map<String, dynamic> json) {
@@ -204,22 +221,30 @@ Widget getChildrenByLayoutMode(Map<String, dynamic> json) {
 getCrossAxisAlignment(Map<String, dynamic> json) {
   print('getCrossAxisAlignment');
   if (json['constraints'] != null) {
-    switch(json['constraints']['vertical']) {
+    switch (json['constraints']['vertical']) {
       case 'CENTER':
         return CrossAxisAlignment.center;
-      default:
-        return CrossAxisAlignment.start;
     }
   }
+  switch (json['counterAxisAlignItems']) {
+    case 'CENTER':
+      return CrossAxisAlignment.center;
+  }
+  return CrossAxisAlignment.start;
 }
+
 getMainAxisAlignment(Map<String, dynamic> json) {
   print('getCrossAxisAlignment');
   if (json['constraints'] != null) {
-    switch(json['constraints']['horizontal']) {
+    switch (json['constraints']['horizontal']) {
       case 'CENTER':
         return MainAxisAlignment.center;
-      default:
-        return MainAxisAlignment.start;
     }
   }
+  switch (json['primaryAxisAlignItems']) {
+    case 'CENTER':
+      return MainAxisAlignment.center;
+  }
+
+  return MainAxisAlignment.start;
 }
