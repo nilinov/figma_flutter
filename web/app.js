f2f5c7a0e10b484c276ffa98f32dcc3b85ddf103
nilinov <72597726+nilinov@users.noreply.github.com>
2021-05-12 12:05:09 +0700

    ++

--- web/app.js
+++ web/app.js
@@ -1,3 +1,9 @@
 function getData() {
     return window.figma_json ?? '';
 }
+
+window.addEventListener('message', receiver, false);
+function receiver(e) {
+    console.log({message: e})
+    window.figma_json = e;
+}
