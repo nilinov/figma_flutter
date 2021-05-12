f08e5ddc6190a09c61e87b2093f206c84956ce17
nilinov <72597726+nilinov@users.noreply.github.com>
2021-05-12 11:09:13 +0700

    ++

--- web/app.js
+++ web/app.js
@@ -1,12 +1,3 @@
-function alertMessage(text) {
-    alert(text)
-}
-
-window.logger = (flutter_value) => {
-   console.log({ js_context: this, flutter_value });
-}
-
-
 function getData() {
     return window.figma_json ?? '';
 }
