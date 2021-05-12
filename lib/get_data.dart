43b4be8d1696d3df3c2dfebcb1d86c1dd9d80032
nilinov <72597726+nilinov@users.noreply.github.com>
2021-05-12 12:19:48 +0700

    ++

--- lib/get_data.dart
+++ lib/get_data.dart
@@ -1,11 +1,16 @@
+import 'dart:io';
 import 'dart:js' as js;
 
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 Future<String> getData() async {
+
+  return '''
+  {"type":"COMPONENT","id":"9:4","parent":{"id":"0:1"},"name":"Frame 1","removed":false,"visible":true,"locked":false,"opacity":1,"blendMode":"PASS_THROUGH","isMask":false,"effects":[],"effectStyleId":"","relativeTransform":[[1,0,413],[0,1,179]],"absoluteTransform":[[1,0,413],[0,1,179]],"x":413,"y":179,"width":181,"height":248,"rotation":0,"constrainProportions":false,"layoutGrow":0,"children":[{"type":"RECTANGLE","id":"9:5","parent":{"id":"9:4"},"name":"Rectangle 1","removed":false,"visible":true,"locked":false,"opacity":1,"blendMode":"PASS_THROUGH","isMask":false,"effects":[],"effectStyleId":"","fills":[{"type":"SOLID","visible":true,"opacity":1,"blendMode":"NORMAL","color":{"r":0.7686274647712708,"g":0.7686274647712708,"b":0.7686274647712708}}],"fillStyleId":"","strokes":[],"strokeStyleId":"","strokeWeight":1,"strokeAlign":"INSIDE","strokeCap":"NONE","strokeJoin":"MITER","strokeMiterLimit":4,"dashPattern":[],"relativeTransform":[[1,0,10],[0,1,10]],"absoluteTransform":[[1,0,423],[0,1,189]],"x":10,"y":10,"width":161,"height":138,"rotation":0,"layoutAlign":"STRETCH","constrainProportions":false,"layoutGrow":0,"exportSettings":[],"constraints":{"horizontal":"MIN","vertical":"MIN"},"cornerRadius":0,"cornerSmoothing":0,"topLeftRadius":0,"topRightRadius":0,"bottomLeftRadius":0,"bottomRightRadius":0,"reactions":[]},{"type":"TEXT","id":"9:6","parent":{"id":"9:4"},"name":"Utils main.dart imports.dart MainPage.dart","removed":false,"visible":true,"locked":false,"opacity":1,"blendMode":"PASS_THROUGH","isMask":false,"effects":[],"effectStyleId":"","fills":[{"type":"SOLID","visible":true,"opacity":1,"blendMode":"NORMAL","color":{"r":0,"g":0,"b":0}}],"fillStyleId":"","strokes":[],"strokeStyleId":"","strokeWeight":1,"strokeAlign":"OUTSIDE","strokeCap":"NONE","strokeJoin":"MITER","strokeMiterLimit":4,"dashPattern":[],"relativeTransform":[[1,0,10],[0,1,158]],"absoluteTransform":[[1,0,423],[0,1,337]],"x":10,"y":158,"width":79,"height":80,"rotation":0,"layoutAlign":"INHERIT","constrainProportions":false,"layoutGrow":0,"exportSettings":[],"constraints":{"horizontal":"SCALE","vertical":"SCALE"},"textStyleId":"","characters":"Utils\\nmain.dart\\nimports.dart\\nMainPage.dart","hasMissingFont":false,"autoRename":true,"fontSize":12,"paragraphIndent":0,"paragraphSpacing":0,"textAlignHorizontal":"LEFT","textAlignVertical":"TOP","textCase":"ORIGINAL","textDecoration":"NONE","textAutoResize":"WIDTH_AND_HEIGHT","letterSpacing":{"unit":"PERCENT","value":0},"lineHeight":{"unit":"PERCENT","value":169.68750953674316},"reactions":[]}],"exportSettings":[],"fills":[{"type":"SOLID","visible":true,"opacity":1,"blendMode":"NORMAL","color":{"r":1,"g":1,"b":1}}],"fillStyleId":"","strokes":[],"strokeStyleId":"","strokeWeight":1,"strokeAlign":"INSIDE","strokeCap":"NONE","strokeJoin":"MITER","strokeMiterLimit":4,"dashPattern":[],"cornerRadius":0,"cornerSmoothing":0,"topLeftRadius":0,"topRightRadius":0,"bottomLeftRadius":0,"bottomRightRadius":0,"paddingLeft":10,"paddingRight":10,"paddingTop":10,"paddingBottom":10,"primaryAxisAlignItems":"MIN","counterAxisAlignItems":"MIN","primaryAxisSizingMode":"FIXED","layoutGrids":[],"gridStyleId":"","backgrounds":[{"type":"SOLID","visible":true,"opacity":1,"blendMode":"NORMAL","color":{"r":1,"g":1,"b":1}}],"backgroundStyleId":"","clipsContent":false,"guides":[],"expanded":false,"constraints":{"horizontal":"MIN","vertical":"MIN"},"layoutMode":"VERTICAL","counterAxisSizingMode":"FIXED","horizontalPadding":10,"verticalPadding":10,"itemSpacing":10,"overflowDirection":"NONE","numberOfFixedChildren":0,"overlayPositionType":"CENTER","overlayBackground":{"type":"NONE"},"overlayBackgroundInteraction":"NONE","description":"","documentationLinks":[],"remote":false,"key":"706f32939774bb542bddf4f789e349e32aefa74f","reactions":[]}
+''';
+
   var res = js.context.callMethod('getData');
-  while(res.isEmpty) {
+  while (res.isEmpty) {
     print('read data...');
     try {
       res = js.context.callMethod('getData');
@@ -61,11 +66,11 @@ Widget getWidgetByMap(Map<String, dynamic> json) {
 
 EdgeInsets getPadding(Map<String, dynamic> json) {
   return EdgeInsets.only(
-        top: json['paddingTop'],
-        left: json['paddingLeft'],
-        right: json['paddingRight'],
-        bottom: json['paddingBottom'],
-      );
+    top: json['paddingTop'],
+    left: json['paddingLeft'],
+    right: json['paddingRight'],
+    bottom: json['paddingBottom'],
+  );
 }
 
 Border getBorder(Map<String, dynamic> json) {
