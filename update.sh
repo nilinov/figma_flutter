9582959173c5b490969c25837e753d3fafb13e88
Danil Yeremeyev <sorhajy@icloud.com>
2021-06-02 04:46:32 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -e && set -x
 
+# Absolute path to this script. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f $0)
+# Absolute path this script is in. /home/user/bin
+DIR=`dirname $SCRIPT`
+cd $DIR
+
 git fetch origin
 git reset --hard origin/main
 flutter pub get
