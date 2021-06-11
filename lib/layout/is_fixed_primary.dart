e777a14a13a6946b498510b8191a9af1cdf70474
Nicolay <nic.ilinov@gmail.com>
2021-06-11 14:01:51 +0700

    добавил обработку свойства primaryAxisSizingMode == AUTO для колонок для снятия ограничения высоты

--- lib/layout/is_fixed_primary.dart
+++ lib/layout/is_fixed_primary.dart
@@ -37,7 +37,7 @@ bool isExpandedHeight(Json parent, Json json) {
       if (isGrow(json)) {
         return true;
       } else {
-        if (isPrimaryAxisSizingModeAuto(json)) {
+        if (isPrimaryAxisSizingModeAuto(parent)) {
           return true;
         }
       }
@@ -45,7 +45,7 @@ bool isExpandedHeight(Json parent, Json json) {
       if (isStretch(json)) {
         return true;
       } else {
-        if (isPrimaryAxisSizingModeAuto(json)) {
+        if (isPrimaryAxisSizingModeAuto(parent)) {
           return true;
         }
       }
