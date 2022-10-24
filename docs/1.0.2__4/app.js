3ab4e0dd85ac53b234b35ce09236600bbcbd9383
Nikolay Ilinov <nikolay.ilinov@x5.ru>
2022-10-24 11:25:04 +0700

    deploy

---
+++ docs/1.0.2__4/app.js
@@ -0,0 +1,61 @@
+function getData() {
+    return window.figma_json ?? window.localStorage.figma_json ?? '';
+}
+
+console.log('test14')
+
+
+window.addEventListener('load', function(event) {
+    window.postMessage('flutter loaded');
+
+}, false);
+
+window.addEventListener('message', receiver, false);
+
+function receiver(e) {
+    console.log('receiver data in figma plugin app')
+    console.log(e.data)
+    window.figma_json = e.data;
+}
+
+function fallbackCopyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+
+    // Avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Fallback: Copying text command was ' + msg);
+    } catch (err) {
+        console.error('Fallback: Oops, unable to copy', err);
+    }
+
+    document.body.removeChild(textArea);
+}
+
+function fallbackDownloadWidget(text, name) {
+    saveByteArray([text], name);
+}
+
+var saveByteArray = (function () {
+        var a = document.createElement("a");
+        document.body.appendChild(a);
+        a.style = "display: none";
+        return function (data, name) {
+            var blob = new Blob(data, {type: "octet/stream"}),
+                url = window.URL.createObjectURL(blob);
+            a.href = url;
+            a.download = name;
+            a.click();
+            window.URL.revokeObjectURL(url);
+        };
+    }());
